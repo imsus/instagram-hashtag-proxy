d9d9ab932d6ccf51e17bf5350224ebf9299b2774
Imam Susanto <abc.imams@gmail.com>
2019-02-15 10:10:51 +0700

    Add Allow Origin

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ module.exports = (req, res) => {
   const { hashtag = 'luminejakarta' } = query;
   const cacheName = `hashtag:${hashtag}`;
 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200);
+  }
+
   if (!mcache.get(cacheName)) {
     axios
       .get(`https://www.instagram.com/explore/tags/${hashtag}/?__a=1`)
