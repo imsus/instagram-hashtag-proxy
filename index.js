21803e29578b28f726c8737f015fa90af7ed27ce
Imam Susanto <abc.imams@gmail.com>
2019-02-15 09:54:06 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const mcache = require('memory-cache');
+
+module.exports = (req, res) => {
+  const { query } = parse(req.url, true);
+  const { hashtag = 'luminejakarta' } = query;
+  const cacheName = `hashtag:${hashtag}`;
+
+  if (!mcache.get(cacheName)) {
+    axios
+      .get(`https://www.instagram.com/explore/tags/${hashtag}/?__a=1`)
+      .then(result => {
+        mcache.put(cacheName, JSON.stringify(result.data), 300000);
+      });
+  }
+
+  res.end(mcache.get(cacheName));
+};
